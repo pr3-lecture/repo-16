08dbf8351deb9150b1808a93ab2e70936e2ad74a
mtstar2007 <1411880@stud.hs-mannheim.de>
2018-01-09 13:56:25 +0100

    added test cases for checking exceptions

--- crypter/test.c
+++ crypter/test.c
@@ -25,7 +25,7 @@ static char* testDecrypt() {
   char* input = "URFVPJB[]ZN^XBJCEBVF@ZRKMJ";
   char* expectedOutput = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   KEY key;
-  key.chars = "TPERULESTPERULESTPERULESTP";
+  key.chars = "TPERULES";
   char output[strlen(input)+1];
   decrypt(key, input, output);
 
@@ -33,9 +33,57 @@ static char* testDecrypt() {
   return 0;
 }
 
+static char* testKeyTooShort() {
+  char* input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  KEY key;
+  key.chars = "T";
+  char output[strlen(input)+1];
+  int error = encrypt(key, input, output);
+
+  mu_assert("Key too short!", error = E_KEY_TOO_SHORT);
+  return 0;
+}
+
+static char* testKeyIllegalChar() {
+  char* input = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  KEY key;
+  key.chars = "$%&/()";
+  char output[strlen(input)+1];
+  int error = encrypt(key, input, output);
+
+  mu_assert("Illegal Key!", error = E_KEY_ILLEGAL_CHAR);
+  return 0;
+}
+
+static char* testMessageIllegalChar() {
+  char* input = "ABCDEFGHIJ______QRSTUVWXYZ";
+  KEY key;
+  key.chars = "TPERULES";
+  char output[strlen(input)+1];
+  int error = encrypt(key, input, output);
+
+  mu_assert("Illegal Message!", error = E_MESSAGE_ILLEGAL_CHAR);
+  return 0;
+}
+
+static char* testCypherIllegalChar() {
+  char* input = "ABCDEFGH§%&/()OPQRSTUVWXYZ";
+  KEY key;
+  key.chars = "TPERULES";
+  char output[strlen(input)+1];
+  int error = encrypt(key, input, output);
+
+  mu_assert("Illegal Cypher!", error = E_CYPHER_ILLEGAL_CHAR);
+  return 0;
+}
+
 static char* allTests() {
   mu_run_test(testEncrypt);
   mu_run_test(testDecrypt);
+  mu_run_test(testKeyTooShort);
+  mu_run_test(testKeyIllegalChar);
+  mu_run_test(testMessageIllegalChar);
+  mu_run_test(testCypherIllegalChar);
   return 0;
 }
 
